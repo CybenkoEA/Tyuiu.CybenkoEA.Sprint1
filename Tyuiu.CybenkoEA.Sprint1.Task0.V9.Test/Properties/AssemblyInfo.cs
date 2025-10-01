8990a8b8e4bdf1d9c0a5c5f3462157936f347901
KonevaDD <Tyuiu.KonevaDD@outlook.com>
2025-10-01 21:20:05 +0500

    Task0

---
+++ Tyuiu.CybenkoEA.Sprint1.Task0.V9.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.CybenkoEA.Sprint1.Task0.V9.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.CybenkoEA.Sprint1.Task0.V9.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8ebfeef1-22d9-4128-b501-f2d3b8abd9b6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
