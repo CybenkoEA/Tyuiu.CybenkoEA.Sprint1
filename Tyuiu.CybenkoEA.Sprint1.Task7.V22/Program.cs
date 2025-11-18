7567d0d59e715173759fc618b70b72221a5344f1
alexe <alexe@ALEXEY52NGG>
2025-11-18 15:55:00 +0500

    Task7

---
+++ Tyuiu.CybenkoEA.Sprint1.Task7.V22/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.CybenkoEA.Sprint1.Task7.V22.Lib;
+namespace Tyuiu.CybenkoEA.Sprint1.Task7.V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Цыбенко Е.А. | ИИПБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Цыбенко Е.А. | ИИПБ-25-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое                    *");
+            Console.WriteLine("* выражение по исходным значениям данных, вводимых пользователем.         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x;
+            double y;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadKey();
+        }
+    }
+
+
+}
