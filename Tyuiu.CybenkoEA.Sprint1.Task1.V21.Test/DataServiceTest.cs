950b5a61f0a7979bebd5c3740908d8290cd99a8b
alexe <alexe@ALEXEY52NGG>
2025-11-18 14:06:13 +0500

    Task1

---
+++ Tyuiu.CybenkoEA.Sprint1.Task1.V21.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.CybenkoEA.Sprint1.Task1.V21.Lib;
+namespace Tyuiu.CybenkoEA.Sprint1.Task1.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 3;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ Tyuiu.CybenkoEA.Sprint1.Task1.V21/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.CybenkoEA.Sprint1.Task1.V21.Lib;
+namespace Tyuiu.CybenkoEA.Sprint1.Task1.V21
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Цыбенко Е. А. | ИИПБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы по С#                                       *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Цыбенко Евгений Андреевич | ИИПБ-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать консольную программу которая вычисляет выражение (x*y)/(3+y)   *");
+            Console.WriteLine("* и печатает результат на экране                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double x, y;
+            Console.WriteLine("Введите значение x");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение y");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("РЕЗУЛЬТАТ                                                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadLine();
+        }
+    }
+
+
+
+
+
+
+}
