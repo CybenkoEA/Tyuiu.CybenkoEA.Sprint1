61feb93710816275e401225d34e038c20ea38df6
alexe <alexe@ALEXEY52NGG>
2025-11-18 15:37:00 +0500

    Task6

---
+++ Tyuiu.CybenkoEA.Sprint1.Task6.V14.Test/Dadasda.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.CybenkoEA.Sprint1.Task6.V14.Lib;
+namespace Tyuiu.CybenkoEA.Sprint1.Task6.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            string a = "A a";
+            bool res = ds.CheckLowerCaseRusLetters(a);
+            bool wait = false;
+            Assert.AreEqual(res, wait);
+
+        }
+    }
+}
